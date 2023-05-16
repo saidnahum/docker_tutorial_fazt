007763317b3d3b346e3bc064cdbcdf3f7f7622f4
Said Nahúm <saidnahum@gmail.com>
2023-05-16 20:50:31 +0000

    Update

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import { v4 } from 'uuid';
 
 const port = 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('Hello world')
+   res.json({
+      id: v4()
+   })
 })
 
 app.listen(port)
